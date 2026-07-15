d9cc69f8c9b73090f04005cf2119be67f78d0dfe
minuradesilva2000 <minuradesilva2000@gmail.com>
2026-07-15 16:21:19 +0530

    test: verify Job table headers are displayed correctly

--- cypress/e2e/Jobpage.cy.js
+++ cypress/e2e/Jobpage.cy.js
@@ -19,5 +19,21 @@ describe('Job page Navigation Test Suite', () => {
     cy.contains('Job List',{timeout:10000}).should('be.visible')
 
   })
+  it('should display the correct table headers', () => {
+  cy.contains('button', 'View').should('be.visible').click()
+  cy.get('table.data-table',{timeout:15000}).should('be.visible')
+  cy.get('table.data-table thead th').eq(0).should('have.text','')
+  cy.get('table.data-table thead th').eq(1).should('have.text','Job ID')
+  cy.get('table.data-table thead th').eq(2).should('have.text','Job No')
+  cy.get('table.data-table thead th').eq(3).should('have.text','Job Description')
+  cy.get('table.data-table thead th').eq(4).should('have.text','Job Date')
+  cy.get('table.data-table thead th').eq(5).should('have.text','Delivery Date')
+  cy.get('table.data-table thead th').eq(6).should('have.text','Actual Date')
+  cy.get('table.data-table thead th').eq(7).should('have.text','Qty')
+  cy.get('table.data-table thead th').eq(8).should('have.text','Customer ID')
+  cy.get('table.data-table thead th').eq(9).should('have.text','Priority')
+  cy.get('table.data-table thead th').eq(10).should('have.text','Status')
 
+
+})
 })
