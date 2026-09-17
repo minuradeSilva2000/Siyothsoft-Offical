8e0c183ee72ee0aeefb28ca2d9e32f79f5038c40
minuradesilva2000 <minuradesilva2000@gmail.com>
2026-09-17 23:48:56 +0530

    Add test to verify Reason selection and modal

--- cypress/e2e/Lookup.cy.js
+++ cypress/e2e/Lookup.cy.js
@@ -22,6 +22,17 @@ describe('Lookup page Navigation Test Suite', () => {
     cy.get('select').select('Holiday')
     cy.get('table', { timeout: 15000 }).should('be.visible')
     cy.get('table tbody tr', { timeout: 15000 }).should('have.length.greaterThan', 0)
+    cy.wait(2000)
+  })
+   it('Verify Reason selection displays the Reason table and moda',()=>{
+    cy.visit('https://devflexi.siyothsoft.com/lookups')
+    cy.get('select', { timeout: 15000 }).should('be.visible')
+    cy.get('select').contains('option', 'Reason', { timeout: 15000 }).should('exist')
+    cy.get('select').select('Reason')
+    cy.get('table', { timeout: 15000 }).should('be.visible')
+    cy.get('table tbody tr', { timeout: 15000 }).should('have.length.greaterThan', 0)
+    cy.get('table tbody tr td span', { timeout: 15000 }).first().click()
+    cy.get('.modal-box').should('be.visible')
 
   })
 })
